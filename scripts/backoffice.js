94561245a3f760e2f541aa4a320cf13eb6ee3916
nicolorsillo <nicolorsillo@hotmail.it>
2026-04-17 16:43:55 +0200

    Added confirm button

--- scripts/backoffice.js
+++ scripts/backoffice.js
@@ -1,5 +1,4 @@
 const productURL = "https://striveschool-api.herokuapp.com/api/product";
-const allTheParameters = new URLSearchParams(location.search);
 let productID = "";
 
 class Product {
@@ -15,46 +14,60 @@ class Product {
 const form = document.getElementById("product-form");
 form.addEventListener("submit", function (e) {
   e.preventDefault();
-  const nameInput = document.getElementById("name");
-  const descriptionInput = document.getElementById("description");
-  const brandInput = document.getElementById("brand");
-  const imageUrlInput = document.getElementById("imageUrl");
-  const priceInput = document.getElementById("price");
-  const name = nameInput.value;
-  const description = descriptionInput.value;
-  const brand = brandInput.value;
-  const imageUrl = imageUrlInput.value;
-  const price = priceInput.value;
-  console.log(name, description, brand, imageUrl, price);
-  const nuovoProdotto = new Product(name, description, brand, imageUrl, price);
-  console.log("PRODOTTO RECUPERATO DAL FORM", nuovoProdotto);
 
-  let urlToUse = productURL;
-  if (productID) {
-    urlToUse = productURL + "/" + productID;
-  }
+  const msg = productID
+    ? "Confermi la modifica del prodotto?"
+    : "Confermi il salvataggio del nuovo prodotto?";
 
-  fetch(urlToUse, {
-    method: productID ? "PUT" : "POST",
-    body: JSON.stringify(nuovoProdotto),
-    headers: {
-      "Content-Type": "application/json",
-      Authorization:
-        "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2OWUxZTk5YjczOWY4NzAwMTU3YWIwOGQiLCJpYXQiOjE3NzY0MTMwODMsImV4cCI6MTc3NzYyMjY4M30.7EscX5UwqW8GsCyztZ2BkfjREjggPTDBnflaaczYOI8",
-    },
-  })
-    .then((response) => {
-      if (response.ok) {
-        alert("PRODOTTO SALVATO!");
-        form.reset();
-        location.reload();
-      } else {
-        throw new Error("Il server ha rifiutato il prodotto");
-      }
+  if (confirm(msg)) {
+    const nameInput = document.getElementById("name");
+    const descriptionInput = document.getElementById("description");
+    const brandInput = document.getElementById("brand");
+    const imageUrlInput = document.getElementById("imageUrl");
+    const priceInput = document.getElementById("price");
+    const name = nameInput.value;
+    const description = descriptionInput.value;
+    const brand = brandInput.value;
+    const imageUrl = imageUrlInput.value;
+    const price = priceInput.value;
+    console.log(name, description, brand, imageUrl, price);
+
+    const nuovoProdotto = new Product(
+      name,
+      description,
+      brand,
+      imageUrl,
+      price,
+    );
+    console.log("PRODOTTO RECUPERATO DAL FORM", nuovoProdotto);
+
+    let urlToUse = productURL;
+    if (productID) {
+      urlToUse = productURL + "/" + productID;
+    }
+
+    fetch(urlToUse, {
+      method: productID ? "PUT" : "POST",
+      body: JSON.stringify(nuovoProdotto),
+      headers: {
+        "Content-Type": "application/json",
+        Authorization:
+          "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2OWUxZTk5YjczOWY4NzAwMTU3YWIwOGQiLCJpYXQiOjE3NzY0MTMwODMsImV4cCI6MTc3NzYyMjY4M30.7EscX5UwqW8GsCyztZ2BkfjREjggPTDBnflaaczYOI8",
+      },
     })
-    .catch((err) => {
-      console.log("SALVATAGGIO PRODOTTO FALLITO", err);
-    });
+      .then((response) => {
+        if (response.ok) {
+          alert("PRODOTTO SALVATO!");
+          form.reset();
+          location.reload();
+        } else {
+          throw new Error("Il server ha rifiutato il prodotto");
+        }
+      })
+      .catch((err) => {
+        console.log("SALVATAGGIO PRODOTTO FALLITO", err);
+      });
+  }
 });
 
 const footerYear = function () {
@@ -76,55 +89,59 @@ const getProducts = function () {
         throw new Error("Response NON ok");
       }
     })
+
     .then((data) => {
       document.getElementById("spinner-section").classList.add("d-none");
-
-      console.log("ARRAY DI PRODOTTI ESISTENTI", data);
+      const list = document.getElementById("product-list");
       data.forEach((productObject) => {
         const newCol = document.createElement("div");
-        newCol.classList.add("col");
         newCol.innerHTML = `
-          <div class="card mb-3" id="${productObject._id}">
-      <div class="row g-0">
-        <div class="col-md-4 d-flex justify-content-center">
-          <img
-            src="${productObject.imageUrl}"
-            class="img-fluid rounded-start object-fit-contain"
-            alt="product picture"
-            style="max-height: 200px"
-          />
-        </div>
-        <div class="col-md-6">
-          <div
-            class="card-body h-100 d-flex flex-column justify-content-between"
-          >
-            <div>
-              <h6 class="card-title">${productObject.brand}</h6>
-              <h4 class="card-title">${productObject.name}</h4>
-              <p class="card-text">${productObject.description}</p>
+            <div class="card mb-3" id="${productObject._id}">
+              <div class="row g-0">
+                <div class="col-md-4 d-flex justify-content-center">
+                  <img
+                    src="${productObject.imageUrl}"
+                    class="img-fluid rounded-start object-fit-contain"
+                    alt="product picture"
+                    style="max-height: 200px"
+                  />
+                </div>
+                <div class="col-md-6">
+                  <div
+                    class="card-body h-100 d-flex flex-column justify-content-between"
+                  >
+                    <div>
+                      <h6 class="card-title">${productObject.brand}</h6>
+                      <h4 class="card-title">${productObject.name}</h4>
+                      <p class="card-text">${productObject.description}</p>
+                    </div>
+                    <div>
+                      <p class="card-text mt-auto">
+                        <small class="text-body-secondary"
+                          >Price = ${productObject.price}€</small
+                        >
+                      </p>
+                    </div>
+                  </div>
+                </div>
+                <div class="col-md-2 d-flex flex-column justify-content-sm-around px-3">
+                  <button
+                    class="btn btn-info"
+                    onclick="modifyProduct('${productObject._id}')"
+                  >
+                    Modifica<i class="bi bi-pencil-fill ms-2"></i>
+                  </button>
+                  <button
+                    class="btn btn-danger"
+                    onclick="deleteProduct('${productObject._id}')"
+                  >
+                    Elimina<i class="bi bi-trash ms-2"></i>
+                  </button>
+                </div>
+              </div>
             </div>
-            <div>
-              <p class="card-text mt-auto">
-                <small class="text-body-secondary"
-                  >Price = ${productObject.price}€</small
-                >
-              </p>
-            </div>
-          </div>
-        </div>
-        <div class="col-md-2 d-flex flex-column justify-content-sm-around px-3">
-          <button class="btn btn-info" onclick="modifyProduct('${productObject._id}')">
-            Modifica<i class="bi bi-pencil-fill ms-2"></i>
-          </button>
-          <button class="btn btn-danger" onclick="deleteProduct('${productObject._id}')">
-            Elimina<i class="bi bi-trash ms-2"></i>
-          </button>
-        </div>
-      </div>
-    </div>
         `;
-        const row = document.getElementById("product-list");
-        row.appendChild(newCol);
+        list.appendChild(newCol);
       });
     })
     .catch((error) => {
@@ -133,72 +150,52 @@ const getProducts = function () {
 };
 
 const deleteProduct = function (productId) {
-  fetch(productURL + "/" + productId, {
-    method: "DELETE",
-    headers: {
-      Authorization:
-        "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2OWUxZTk5YjczOWY4NzAwMTU3YWIwOGQiLCJpYXQiOjE3NzY0MTMwODMsImV4cCI6MTc3NzYyMjY4M30.7EscX5UwqW8GsCyztZ2BkfjREjggPTDBnflaaczYOI8",
-    },
-  })
-    .then((response) => {
-      if (response.ok) {
-        alert("PRODOTTO ELIMINATO!");
-        document.getElementById(productId).classList.add("d-none");
-      } else {
-        throw new Error("errore nell'eliminazione del prodotto!");
-      }
-    })
-    .catch((err) => {
-      console.log("ERRORE FETCH", err);
-    });
-};
-
-footerYear();
-getProducts();
-
-const modifyProduct = function (productId) {
-  {
+  if (confirm("Sei sicuro di voler eliminare questo prodotto?")) {
     fetch(productURL + "/" + productId, {
+      method: "DELETE",
       headers: {
         Authorization:
           "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2OWUxZTk5YjczOWY4NzAwMTU3YWIwOGQiLCJpYXQiOjE3NzY0MTMwODMsImV4cCI6MTc3NzYyMjY4M30.7EscX5UwqW8GsCyztZ2BkfjREjggPTDBnflaaczYOI8",
       },
     })
-      .then((response) => {
-        if (response.ok) {
-          return response.json();
-        } else {
-          throw new Error("Errore nel recupero dettagli prodotto");
+      .then((res) => {
+        if (res.ok) {
+          alert("ELIMINATO!");
+          document.getElementById(productId).remove();
         }
       })
-      .then((details) => {
-        const btnSalva = document.getElementById("btn-salva");
-        const nameInput = document.getElementById("name");
-        const descriptionInput = document.getElementById("description");
-        const brandInput = document.getElementById("brand");
-        const imageUrlInput = document.getElementById("imageUrl");
-        const priceInput = document.getElementById("price");
-        nameInput.value = details.name;
-        descriptionInput.value = details.description;
-        brandInput.value = details.brand;
-        imageUrlInput.value = details.imageUrl;
-        priceInput.value = details.price;
-        btnSalva.innerText = "MODIFICA";
-      })
-      .catch((err) => {
-        console.log("ERRORE NEL RIPOPOLAMENTO DEL FORM", err);
-      });
+      .catch((err) => console.log(err));
   }
-
-  window.scrollTo({
-    top: 0,
-    behavior: "smooth",
-  });
-  productID = productId;
 };
 
-const resetForm = function () {
-  productID = "";
-  const btnSalva = document.getElementById("btn-salva");
-  btnSalva.innerText = "SALVA";
+const modifyProduct = function (productId) {
+  fetch(productURL + "/" + productId, {
+    headers: {
+      Authorization:
+        "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2OWUxZTk5YjczOWY4NzAwMTU3YWIwOGQiLCJpYXQiOjE3NzY0MTMwODMsImV4cCI6MTc3NzYyMjY4M30.7EscX5UwqW8GsCyztZ2BkfjREjggPTDBnflaaczYOI8",
+    },
+  })
+    .then((res) => res.json())
+    .then((details) => {
+      document.getElementById("name").value = details.name;
+      document.getElementById("description").value = details.description;
+      document.getElementById("brand").value = details.brand;
+      document.getElementById("imageUrl").value = details.imageUrl;
+      document.getElementById("price").value = details.price;
+      document.getElementById("btn-salva").innerText = "MODIFICA";
+      productID = productId;
+      window.scrollTo({ top: 0, behavior: "smooth" });
+    });
 };
+
+const resetForm = function (e) {
+  if (confirm("Vuoi svuotare il form?")) {
+    productID = "";
+    document.getElementById("btn-salva").innerText = "SALVA";
+  } else {
+    e.preventDefault();
+  }
+};
+
+footerYear();
+getProducts();
